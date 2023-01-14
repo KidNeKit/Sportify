66a3ebb419941cb93c43da7d0ae2ffab8cd59c95
Nikita Menshikov <menshikov.mikita@gmail.com>
2023-01-14 17:19:53 +0300

    Added user creation in firestore

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sportify/models/user.dart';
 
 class AuthRepository {
   final auth.FirebaseAuth _authRepository;
@@ -29,6 +31,13 @@ class AuthRepository {
       final credential = await _authRepository.createUserWithEmailAndPassword(
           email: email, password: password);
       final user = credential.user;
+
+      User myUser = User(id: user!.uid, email: user.email!);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .set(myUser.toMap());
+
       return user;
     } catch (e) {
       log('Error due user registration: $e');
