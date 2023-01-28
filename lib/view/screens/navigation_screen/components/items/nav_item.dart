ddebb25bf2d746ea440c294c40dec0da2946df0f
Nikita Menshikov <menshikov.mikita@gmail.com>
2023-01-28 14:00:28 +0300

    Animation working correctly

--- lib/view/screens/navigation_screen/components/items/nav_item.dart
+++ lib/view/screens/navigation_screen/components/items/nav_item.dart
@@ -6,7 +6,7 @@ import 'package:sportify/cubits/navigation/navigation_cubit.dart';
 
 import '../../../../../models/nav_icon.dart';
 
-class NavItem extends StatefulWidget {
+class NavItem extends StatelessWidget {
   final NavIcon _navIcon;
   final int _index;
 
@@ -15,38 +15,29 @@ class NavItem extends StatefulWidget {
         _index = index;
 
   @override
-  State<NavItem> createState() => _NavItemState();
-}
-
-class _NavItemState extends State<NavItem> {
-  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        AnimatedContainer(
-          duration: const Duration(seconds: 2),
-          curve: Curves.easeIn,
-          child: BlocBuilder<NavigationCubit, NavigationState>(
-            builder: (context, state) {
-              log('rebuilding icon ${widget._navIcon.label}');
-              return Container(
-                height: 4,
-                width: state.index == widget._index ? 20.0 : 0.0,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              );
-            },
+        BlocBuilder<NavigationCubit, NavigationState>(
+          builder: (context, state) => AnimatedContainer(
+            width: state.index == _index ? 20.0 : 0.0,
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeIn,
+            child: Container(
+              height: 4,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
           ),
         ),
         const SizedBox(height: 5.0),
         GestureDetector(
-            onTap: () =>
-                context.read<NavigationCubit>().changeScreen(widget._index),
-            child: Icon(widget._navIcon.icon)),
+            onTap: () => context.read<NavigationCubit>().changeScreen(_index),
+            child: Icon(_navIcon.icon)),
       ],
     );
   }
