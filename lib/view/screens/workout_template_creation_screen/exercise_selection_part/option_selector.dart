d438dbed97c9fc9433beb04ce8a20df4134d4f70
Nikita Menshikov <menshikov.mikita@gmail.com>
2023-02-07 20:06:08 +0300

    Added number of selected exercises near with 'selected' option

--- lib/view/screens/workout_template_creation_screen/exercise_selection_part/option_selector.dart
+++ lib/view/screens/workout_template_creation_screen/exercise_selection_part/option_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sportify/cubits/exercise_template/exercise_template_cubit.dart';
 
 import 'option.dart';
 
@@ -18,47 +20,72 @@ class _OptionSelectorState extends State<OptionSelector> {
     double screenWidth = MediaQuery.of(context).size.width;
     return Container(
       margin: EdgeInsets.symmetric(horizontal: marginHor),
-      child: Stack(children: [
-        Container(
-          height: 50,
-          width: screenWidth,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-        ),
-        AnimatedContainer(
-          margin: _showAll
-              ? EdgeInsets.zero
-              : EdgeInsets.only(left: ((screenWidth - 2 * marginHor) / 2)),
-          duration: const Duration(milliseconds: 200),
-          child: Container(
+      child: Stack(
+        children: [
+          Container(
             height: 50,
-            width: (screenWidth - 2 * marginHor) / 2,
+            width: screenWidth,
             decoration: BoxDecoration(
+              color: Colors.grey,
               borderRadius: BorderRadius.circular(20.0),
-              color: Colors.purple,
             ),
           ),
-        ),
-        SizedBox(
-          height: 50.0,
-          child: Row(
-            children: [
-              Option(
-                label: 'Show All',
-                isActive: _showAll,
-                onSelectOptionFunc: () => setState(() => _showAll = !_showAll),
+          AnimatedContainer(
+            margin: _showAll
+                ? EdgeInsets.zero
+                : EdgeInsets.only(left: ((screenWidth - 2 * marginHor) / 2)),
+            duration: const Duration(milliseconds: 200),
+            child: Container(
+              height: 50,
+              width: (screenWidth - 2 * marginHor) / 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.purple,
               ),
-              Option(
-                label: 'Show Selected',
-                isActive: !_showAll,
-                onSelectOptionFunc: () => setState(() => _showAll = !_showAll),
-              ),
-            ],
+            ),
           ),
-        ),
-      ]),
+          SizedBox(
+            height: 50.0,
+            child: Row(
+              children: [
+                Option(
+                  label: 'Show All',
+                  isActive: _showAll,
+                  onSelectOptionFunc: () =>
+                      setState(() => _showAll = !_showAll),
+                ),
+                Option(
+                  label: 'Show Selected',
+                  isActive: !_showAll,
+                  onSelectOptionFunc: () =>
+                      setState(() => _showAll = !_showAll),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Container(
+              padding: const EdgeInsets.all(6.0),
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                shape: BoxShape.circle,
+              ),
+              child: BlocBuilder<ExerciseTemplateCubit, ExerciseTemplateState>(
+                buildWhen: (previous, current) =>
+                    previous.templates.length != current.templates.length,
+                builder: (context, state) {
+                  return Text(
+                    state.templates.length.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
